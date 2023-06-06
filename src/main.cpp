4b2779bfa6dc86ac5c60970a7a05ccd1ed9c661e
luishgh <51053603+luishgh@users.noreply.github.com>
2023-06-06 12:07:29 -0300

    Test armadillo

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,9 @@
 int main() {
   std::cout << "Olá" << std::endl;
   std::cout << "Armadillo version: " << arma::arma_version::as_string() << std::endl;
+  arma::mat A(4, 5, arma::fill::randu);
+  arma::mat B(4, 5, arma::fill::randu);
+
+  std::cout << A*B.t() << std::endl;
   return 0;
 }
