cdab8dc610ababc0a67973efe4d454bffa140687
luishgh <51053603+luishgh@users.noreply.github.com>
2023-06-27 22:48:19 -0300

    Add doxygen docs

--- lib/lsa.hpp
+++ lib/lsa.hpp
@@ -6,11 +6,16 @@
 #include "document.hpp"
 #include "ranking.hpp"
 
+//! \brief Classe que implementa o modelo de ranqueamento de relevância LSA/LSI.
+/*!
+  Esse modelo utiliza da fatoração de matrizes SVD para melhorar os resultados do ranqueamento em relação ao modelo vetorial. Para mais detalhes, consulte a wiki do projeto no Github.
+ */
 class LsaRanking : public Ranking {
 private:
   void get_rr_term_document_mat(Weighting & weighter, Eigen::MatrixXd & T, Eigen::VectorXd & s, Eigen::MatrixXd & D) const;
   Eigen::VectorXd get_query_vec(Weighting & weighter, std::string query) const;
 public:
+  //! Constroe uma instância do modelo LSA/LSI para um dado conjunto de documentos
   LsaRanking(DocumentsData & data, DocumentIndex & index) : Ranking(data, index) {}
   std::vector<int> rank(Weighting & weighter, std::string query) const override;
 };
--- lib/vector_space.hpp
+++ lib/vector_space.hpp
@@ -6,10 +6,15 @@
 #include "ranking.hpp"
 #include "document.hpp"
 
+//! \brief Classe que implementa o modelo vetorial de ranqueamento de relevância.
+/*!
+  Esse modelo representa os documentos e a busca como vetores em um espaço vetorial dos termos, utilizando do ângulo entre eles como um indicador de relevância/similaridade. Para mais detalhes, consulte a wiki do projeto no Github.
+*/
 class VectorSpaceRanking : public Ranking {
 private:
   Eigen::VectorXd get_query_vec(Weighting & weighter, std::string query) const;
 public:
+  //! Constroe uma instância do modelo vetorial de ranqueamento para um dado conjunto de documentos
   VectorSpaceRanking(DocumentsData & data, DocumentIndex & index) : Ranking(data, index) {}
   std::vector<int> rank(Weighting & weighter, std::string query) const override;
 };
