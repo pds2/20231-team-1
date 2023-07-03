61c6797d24c0b20b3fbf6af6e88366015419cd9b
Julio <jgdjulio@gmail.com>
2023-07-03 18:54:02 -0300

    document.hpp update

--- lib/document.hpp
+++ lib/document.hpp
@@ -20,10 +20,10 @@ private:
     std::unordered_map<int, DocMetadata> doc_metadata;
 public:
     DocumentsData();
-    int get_size(int doc_idx);
-    double get_avg_size();
-    int get_qt_docs();
-    int get_frequence(std::string term, int doc_idx);
+    int get_size(int doc_idx); // número de palavras no arquivo
+    double get_avg_size(); // número de palavras médio, considerando todos os arquivos
+    int get_qt_docs(); // número de documentos na pasta input
+    int get_frequence(std::string term, int doc_idx); // frequência de cada termo no arquivo
 }
 
 #endif
