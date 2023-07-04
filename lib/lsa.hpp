97957830d3e3bfab19f8311585eaad55357e33b1
luishgh <51053603+luishgh@users.noreply.github.com>
2023-07-04 10:19:52 -0300

    Update docs

--- lib/lsa.hpp
+++ lib/lsa.hpp
@@ -6,17 +6,25 @@
 #include "document.hpp"
 #include "ranking.hpp"
 
-//! \brief Classe que implementa o modelo de ranqueamento de relevância LSA/LSI.
-/*!
-  Esse modelo utiliza da fatoração de matrizes SVD para melhorar os resultados do ranqueamento em relação ao modelo vetorial. Para mais detalhes, consulte a wiki do projeto no Github.
+/**
+ * @brief Classe que implementa o modelo de ranqueamento de relevância LSA/LSI.
+ *
+ * Esse modelo utiliza da fatoração de matrizes SVD para melhorar os resultados do ranqueamento em relação ao modelo vetorial. Para mais detalhes, consulte a wiki do projeto no Github.
  */
 class LsaRanking : public Ranking {
 private:
   void get_rr_term_document_mat(Weighting & weighter, Eigen::MatrixXd & T, Eigen::VectorXd & s, Eigen::MatrixXd & D) const;
   Eigen::VectorXd get_query_vec(Weighting & weighter, std::string query) const;
 public:
-  //! Constroe uma instância do modelo LSA/LSI para um dado conjunto de documentos
+  /**
+   * @brief Constroe uma instância do modelo LSA de ranqueamento para um dado corpus
+   */
   LsaRanking(DocumentsData & data, DocumentIndex & index) : Ranking(data, index) {}
+  /**
+   * @brief Ranquea os documentos de acordo com a relevância para a query
+   * @param weighter O modelo de ponderamento a ser utilizado
+   * @param query A busca do usuário
+   */
   std::vector<int> rank(Weighting & weighter, std::string query) const override;
 };
 
--- lib/ranking.hpp
+++ lib/ranking.hpp
@@ -4,18 +4,24 @@
 #include "document.hpp"
 #include "weighting.hpp"
 
-//! Classe abstrata que serve de interface para diferentes modelos de ranqueamento de relevância
+//! @brief Classe abstrata que serve de interface para diferentes modelos de ranqueamento de relevância
 class Ranking {
 protected:
+  //! Dados gerais sobre o corpus a ser ranqueado
   DocumentsData & data;
+  //! A lista invertida do corpus
   DocumentIndex & index;
 public:
-  //! Constroe uma instância do modelo de ranqueamento para um dado conjunto de documentos
+  /**
+   * @brief Constroe uma instância do modelo de ranqueamento para um dado conjunto de documentos
+   * @param data Dados gerais sobre o corpus a ser ranqueado
+   * @param index A lista invertida do corpus
+   */
   Ranking(DocumentsData & data, DocumentIndex & index) : data(data), index(index) {}
-  //! \brief Ranquea os documentos de acordo com a relevância para a query
-  /*!
-    \param weightner o modelo de ponderamento a ser utilizado
-    \param query a busca do usuário
+  /**
+   * @brief Ranquea os documentos de acordo com a relevância para a query
+   * @param weightner o modelo de ponderamento a ser utilizado
+   * @param query a busca do usuário
    */
   virtual std::vector<int> rank(Weighting & weightner, std::string query) const = 0;
 };
