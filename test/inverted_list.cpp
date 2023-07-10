790ec821662a7273b2c6c41f1b8402b4642a992a
samuelsnv2 <samuelsnv2@gmail.com>
2023-07-10 15:03:48 -0300

    More tests of documents, 07 incomplete

--- test/inverted_list.cpp
+++ test/inverted_list.cpp
@@ -15,7 +15,7 @@ namespace fs = std::filesystem;
 using DocumentNames = std::map<std::string, std::vector<std::string>>;
 
 TEST_CASE("01 - Inverted List Test without a valid dir") {
-    DOCTEST_CHECK_THROWS_AS(DocumentsData("../include/"), dir_not_found_e);
+    DOCTEST_CHECK_THROWS_AS(DocumentsData("../include/"), DirNotFoundException);
 }
 
 // Compare two index:
@@ -77,6 +77,8 @@ TEST_CASE("03 - Test avarage size of documents in a temporary directory") {
     int size3 = data.get_size(2);
     double mean_size = (size1 + size2 + size3) / 3;
     CHECK(size == mean_size);
+
+    fs::remove_all(tmp_dir);
 }
 
 TEST_CASE("04 - Test get frequency of terms in documents of a temporary directory") {
@@ -98,4 +100,69 @@ TEST_CASE("04 - Test get frequency of terms in documents of a temporary director
             CHECK(freq == 2);
         }
     }
+
+    fs::remove_all(tmp_dir);
 }
+
+TEST_CASE("05 - Test avarage size of a temporary directory without files") {
+    fs::path tmp_dir = fs::temp_directory_path() / "test_inverted_list";
+    std::map<std::string, std::string> temp_corpus{};
+    utils::create_temp_corpus(tmp_dir, temp_corpus);
+    
+    DocumentsData data(tmp_dir.c_str());
+
+    double size = data.get_avg_size();
+    CHECK(size == 0);
+
+    fs::remove_all(tmp_dir);
+}
+
+TEST_CASE("06 - Test size of a empty document in a temporary directory") {
+    fs::path tmp_dir = fs::temp_directory_path() / "test_inverted_list";
+    std::map<std::string, std::string> temp_corpus{{"0.txt", ""}};
+    utils::create_temp_corpus(tmp_dir, temp_corpus);
+
+    DocumentsData data(tmp_dir.c_str());
+    std::string word = "test";
+
+    int size = data.get_size(0);
+    CHECK(size == 0);
+}
+
+/*
+TEST_CASE("07 - Test conversion from wordIndex to docIndex in a temporary directory") {
+    fs::path tmp_dir = fs::temp_directory_path() / "test_inverted_list";
+    std::map<std::string, std::string> temp_corpus{{"0.txt", "test one, testing"}, {"1.txt", "testing this function"}, {"2.txt", "test three, this is a test"}};
+    utils::create_temp_corpus(tmp_dir, temp_corpus);
+
+    DocumentsData data(tmp_dir.c_str());
+    
+    std::unordered_map<std::string, std::unordered_map<int, int>> wordIndex = {
+        {"test", {{0, 1}, {2, 1}}},
+        {"one", {{0, 1}}},
+        {"testing", {{0, 1}, {1, 1}}},
+        {"this", {{1, 1}, {2, 1}}},
+        {"function", {{1, 1}}},
+        {"three", {{2, 1}}},
+        {"is", {{2, 1}}},
+        {"a", {{2, 1}}},
+    };
+
+    DocumentIndex docIndex = data.convertToDocumentIndex(wordIndex);
+
+    DocumentIndex expectedIndex = {
+        {"test", {0, 2}},
+        {"one", {0}},
+        {"testing", {0, 1}},
+        {"this", {1, 2}},
+        {"function", {1}},
+        {"three", {2}},
+        {"is", {2}},
+        {"a", {2}},
+    };
+
+    CHECK(docIndex == expectedIndex);
+
+    fs::remove_all(tmp_dir);
+}
+*/
